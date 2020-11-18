f705df400ee882f860f5aebda825dd8d227ecb98
pereira frédéric <56920249+pereirafrederic-wip@users.noreply.github.com>
2020-11-18 14:29:27 +0100

    init

---
+++ src/main/java/com/pereirafrederic/projetconfigexternalise/ProjetconfigexternaliseApplication.java
@@ -0,0 +1,13 @@
+package com.pereirafrederic.projetconfigexternalise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetconfigexternaliseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetconfigexternaliseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pereirafrederic/projetconfigexternalise/ProjetconfigexternaliseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pereirafrederic.projetconfigexternalise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetconfigexternaliseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
